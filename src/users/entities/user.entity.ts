be866a2c07f43b88c6084873cda16a2cbfef868d
vergaraCoder <jhonatanvergara072@gmail.com>
2025-01-10 12:37:45 -0500

    feat: creation of user entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,15 @@
+import { Column } from "typeorm";
+
+export class User {
+    @Column()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @Column()
+    email:string;
+
+    @Column()
+    password:string;
+}
